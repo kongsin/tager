0e18b99aca5f7a1111a16dc68ac124239d1ff17d
kognsin <7610tappy>
2016-09-05 03:05:56 +0700

    remove never use module

--- app/src/main/java/com/example/kongsin/autogridlayoutmanager/Interface/OnResponseListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.kongsin.autogridlayoutmanager.Interface;
-
-/**
- * Created by DroidDev on 8/16/16.
- */
-public interface OnResponseListener<T> {
-    void onSuccess(T res);
-    void onFailed(String msg);
-}
