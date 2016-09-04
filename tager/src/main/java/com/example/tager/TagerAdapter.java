7852c504479a7a2f455b2098d656f9707758f0ed
kognsin <7610tappy>
2016-09-04 23:31:12 +0700

    update code

--- tager/src/main/java/com/example/tager/TagerAdapter.java
+++ tager/src/main/java/com/example/tager/TagerAdapter.java
@@ -1,7 +1,6 @@
 package com.example.tager;
 
 import android.support.v7.widget.RecyclerView;
-import android.view.ViewGroup;
 
 /**
  * Created by kognsin on 9/4/2016.
@@ -10,19 +9,8 @@ import android.view.ViewGroup;
 public abstract class TagerAdapter<VH extends RecyclerView.ViewHolder> extends RecyclerView.Adapter<VH> {
 
     @Override
-    public VH onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
-    }
-
-    @Override
     public void onBindViewHolder(VH holder, int position) {
         Tager.getInstance().subscribeView(holder);
         Tager.getInstance().updatePinnedView(holder);
     }
-
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
-
 }
